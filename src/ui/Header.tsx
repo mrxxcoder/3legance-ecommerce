c38d799bc09eb1c047f65fd0bbc7702984fb149f
lowkeylu <ahmedsamy446x@gmail.com>
2023-12-29 23:56:15 +0200

    Fix font size on mobile devices

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -3,12 +3,12 @@ import Button from "./Button";
 function Header() {
   return (
     <div className="h-screen bg-no-repeat bg-cover bg-[url('assets/images/hero-mobile.png')] md:bg-[url('assets/images/hero.png')] px-8 py-12 md:flex items-center justify-end">
-      <div className="flex flex-col space-y-6 items-center md:items-start justify-end md:w-[50%]">
-        <h2 className="font-semibold text-6xl md:text-8xl text-[#121212] text-center md:text-left">
+      <div className="flex flex-col space-y-4 items-center md:items-start justify-end md:w-[50%]">
+        <h2 className="font-semibold text-5xl md:text-8xl text-[#121212] text-left md:text-left">
           Listen to the <span className="text-blue-600">amazing</span> music
           sound.
         </h2>
-        <p className="text-2xl">Experience music like never before</p>
+        <p className="text-xl">Experience music like never before</p>
         <Button width="w-fit" variant="primary">
           Shop now
         </Button>
